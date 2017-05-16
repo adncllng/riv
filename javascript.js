321d1ac914ee84670b2871ea96449ea71f58fcdc
dncllng <aden.collinge@gmail.com>
2017-05-16 11:45:09 -0400

    giving it a go here

---
+++ javascript.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+        $(".info").slimScroll({
+        height: '60vh',
+        color:'#fff'
+    });
+
+    
+    $(".projet").on('click', function(){
+
+    $(".info").html(" <p><h2>Histoire</h2>Côte-des-Neiges.<br><br>Jadis, un little french village de tanneurs et d’agriculteurs, exploitant le courant d’un étroit ruisseau.<br><br> Aujourd’hui, un quartier cosmopolite, où cohabitent étudiants, nouveaux arrivants, et familles. Un territoire dense où circulent des centaines de travailleurs des domaines de la santé et de l’éducation.Une plaque tournante qui abrite toutes les religions du monde, et où 110 langues se font écho. <br><BR>Avec le temps, Côte-des-Neiges est devenu une large rivière, dont le débit a fait affluer des milliers de vies, et où autant d’histoires de migrations se sont versées.<h2>Le Projet</h2>Pour souligner le 375e anniversaire de la ville de Montréal, deux créatrices célébreront le caractère unique de la Côte-des-Neiges en chapeautant le projet Rivières un projet théâtral présenté dans les lieux porteurs d’histoire du quartier.<br><br>Rivières est une série de performances documentaires.<br><br>C’est une tribune, où des citoyens se racontent et se rencontrent. C’est le dévoilement d’une communauté qui se révèle à elle-même. C’est une performance à la croisée des genres, où surgiront les voix de ces voisins inconnus, dans le contexte d’une balade dans le quartier.<br> <br> Rivières fera naviguer ses spectateurs dans les lieux historiques de Côte-des-Neiges, devenu un musée de récits, une galerie de consciences. Réalisé par Sofia Blondin et Veronica Mockler, produit par le Centre communautaire de loisirs de la Côte-des-Neiges, ce spectacle aura lieu à la fin de l’été 2017.<br><br>Il sera offert gratuitement au citoyens.<br><br>Plus de détails viendront. Suivez-nous!</p>");
+        
+    
+               $(".info").css("display", "flex").hide().fadeIn(500);
+        
+        
+    });
+    
+        $(".benefice").on('click', function(){
+ 
+         
+    $(".info").html("<h1>SOIRÉE BÉNÉFICE</h1><p> Côte-à- Côte – Soirée bénéfice Vin, bouchées, et interventions sur le vivre ensemble. Nous voulons que Rivières soit offert gratuitement. Afin de nous encourager à produire un spectacle accessible à un maximum de citoyens, nous vous convions à une Soirée Bénéfice, intitulée Côte-à- Côte. L’achat de votre billet vous donnera accès à un cocktail dînatoire accompagné d’une série d’interventions engagés sur le quartier. Plusieurs acteurs de la communauté, et autres spécialistes seront invités à donner un discours sur le quartier et son histoire, l’engagement social et la responsabilité citoyenne. Il y aura aussi des invités-surprises ! C’est où ? C’est quand ? C’est combien ? De plus amples détails viendront. Restez à l’affût ! VOUS POUVEZ PAS VENIR À LA SOIRÉE BÉNÉFICE ? Et vous voulez nous encourager quand même à produire un spectacle GRATUIT pour la majorité de la communauté ? Lien pour la campagne kickstarter.</p>");
+    
+               $(".info").css("display", "flex").hide().fadeIn(500);
+        
+    });
+        
+        $(".artistes").on('click', function(){
+ 
+         
+    $(".info").html("<h1>LES INTERVENANTS</h1><p>Véronica Mockler fait de la vidéo d’art et de la performance sociale. Après avoir étudié les arts visuels à l’Université Concordia, Véronica a développé une pratique artistique fondée sur la rencontre. Elle aime exposer des individus, et ses œuvres nous donnent accès à leur conscience. L’effet de reconnaissance de l’autre que son travail engendre chez ses spectateurs participe à une cohésion sociale, que Véronica juge essentielle. Ses œuvres ont été exposées à Montréal et Toronto. Elle a également déjà fait une résidence à Dublin. Elle a envie de travailler à Côte-des- Neiges parce que __________. Sofia a gradué en Interprétation de l’École Nationale de théâtre du Canada en 2016. Depuis, cette comédienne a collaboré à plusieurs productions théâtrales et a également eu la chance de se faire connaître par un plus large public dans l’émission District 31. Elle pratique le théâtre dans une perspective de renforcement des liens communautaires, d’où son intérêt pour l’enseignement et son travail avec des amateurs. Elle fait la mise en scène de la troupe étudiante collégiale du Collège Jean-de- Brébeuf, le théâtre du Vaisseau d’Or, enseigne le théâtre et le chant au Camp du conservatoire de McGill, et est chroniqueuse littéraire pour Les Herbes Folles à CISM 89,3 FM. Sofia a pour projet à long terme de créer une communauté théâtrale à Côte-des- Neiges, un quartier qu’elle a longtemps côtoyée en tant qu’étudiante, et maintenant comme artiste. Le Centre communautaire de loisirs de la Côte-des- Neiges fêtera ses 40 ans cette année. Depuis ses débuts, cet organisme bien connu vise à offrir aux citoyens de Côte-des- Neiges un milieu de vie rassembleur, harmonieux, participatif, visant épanouissement personnel et le développement collectif par le loisir, action communautaire et l&#39;éducation populaire. Le CELO est très heureux de coordonner ce projet avec ces deux créatrices.</p>");
+    
+               $(".info").css("display", "flex").hide().fadeIn(500);
+        
+    });
+
+
+});
+    
